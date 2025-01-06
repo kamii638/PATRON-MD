2167f5cdcfe803c8defe01f9785ec66160fa5f49
kamii638 <sainaseer301@gmail.com>
2025-01-06 18:52:34 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 global.bankname = "MONIEPOINT"
 
-global.banknumber = "8133729715"
+global.banknumber = "93081004395"
 
 global.bankowner = "FADARE"
 
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.wlcm = true
 global.autoblockmorroco = true //auto block 212 (true to on, false to off)
 global.autokickmorroco = true //auto kick 212 (true to on, false to off) 
